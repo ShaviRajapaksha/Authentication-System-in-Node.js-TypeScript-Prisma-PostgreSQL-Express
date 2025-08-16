db8f7ea9c5ec547196c8071b04a0efdb4594d184
IT20272654 <shavindurajapaksha@gmail.com>
2025-08-16 20:24:49 +0530

    Create authService

---
+++ src/services/authServices.ts
@@ -0,0 +1,30 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+class AuthService{
+    static registerUser = async (username:string, email: string, password: string) => {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword
+            }
+        });
+        return user;
+    };
+    static findUserById = async(id:number)=>{
+        return await prisma.user.findUnique({
+            where: { id: id }
+        });
+    };
+    findUserByEmail = async(email:string)=>{
+        return await prisma.user.findUnique({
+            where: { email: email }
+        }); 
+    }
+}
+export default AuthService;
