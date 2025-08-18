d99c6f84533dc36e06f0399ca4ba2b3f764ed1e3
IT20272654 <shavindurajapaksha@gmail.com>
2025-08-18 11:31:01 +0530

    Update auth route

--- src/routes/authroutes.ts
+++ src/routes/authroutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import AuthController from '../controllers/authController';
+import AuthMiddleware from '../middlewares/authMiddleware';
 
 const router = Router();
 
 router.post('/auth/register', AuthController.registerUser);
 router.post('/auth/login', AuthController.loginUser);
+router.get('/user/:id', AuthMiddleware.authentication, AuthController.getUserById);
 
 export default router;  
