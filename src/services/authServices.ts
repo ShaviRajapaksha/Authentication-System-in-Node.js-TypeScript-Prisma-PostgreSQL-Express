441dc5c733da1921b107ce05a531f2222b05a410
IT20272654 <shavindurajapaksha@gmail.com>
2025-08-18 10:17:55 +0530

    Update authService

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 import { PrismaClient } from '../generated/prisma';
 
 const prisma = new PrismaClient();
+const JWT_Secret = process.env.JWT_SECRET;
 
 class AuthService{
     static registerUser = async (username:string, email: string, password: string) => {
@@ -26,5 +27,21 @@ class AuthService{
             where: { email: email }
         }); 
     }
+    static loginUser = async (email: string, password: string) => {
+        const user = await AuthService.findUserByEmail(email);
+        if (!user) {
+            throw new Error('User not found');
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            throw new Error('Invalid credentials');
+        }
+        const token = jwt.sign({userId:user.id, username:user.username}, JWT_Secret!, {
+            expiresIn: '1h'
+        });
+        return token;
+    }
+
+
 }
 export default AuthService;
